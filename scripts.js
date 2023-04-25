d1bf7f1850cba92ca7bc1c8a0c978d5b9d125b92
Osman Karabulut <h.osmankarabulut.fb67@hotmail.com>
2023-04-25 12:40:02 +0300

    animation added

--- scripts.js
+++ scripts.js
@@ -6,6 +6,10 @@ function getAbilities() {
   }
 }
 
+function calculate() {
+  var _calc = document.getElementById("calculatenumber");
+  _calc.onclick = calculateInterface;
+}
 function search() {
   var _deger = this.innerHTML;
 
@@ -20,4 +24,43 @@ function search() {
   this.setAttribute("target", "_blank");
 }
 
-getAbilities();
+function getPoint(E, F, G, H, year) {
+  return (
+    (E * Math.pow(year, 3) + F * Math.pow(year, 2) + G * year + H) /
+    Math.pow(year, 4)
+  );
+}
+function getChanceNumber(point) {
+  return 100 * (Math.random() + parseInt(String(point).toString()));
+}
+
+function calculateInterface() {
+  var dt = new Date();
+  var _deger = document.getElementById("fschoolnumber").value;
+
+  var year =
+    parseInt(dt.getUTCFullYear().toString()) - parseInt(_deger.slice(0, 4));
+
+  var E = parseInt(_deger.slice(8, 9));
+  var F = parseInt(_deger.slice(9, 10));
+  var G = parseInt(_deger.slice(10, 11));
+  var H = parseInt(_deger.slice(11, 12));
+
+  var point = getPoint(E, F, G, H, year);
+
+  var change_number = getChanceNumber(point);
+  console.log(point);
+  console.log(change_number);
+  console.log(String(point).slice(0, 2).toString());
+
+  document.getElementById("resultScreen").innerText =
+    "Puan: " + point + "\n Şanslı numara: " + change_number;
+}
+
+function setFunctions() {
+  [getAbilities(), calculate()].forEach((element) => {
+    element();
+  });
+}
+
+setFunctions();
